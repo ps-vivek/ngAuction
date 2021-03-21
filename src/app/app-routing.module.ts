5e64220c0db966c01e72a7631e2deb5f3c303c71
vivek <muralidharan.vivek@outlook.com>
2021-03-21 16:12:14 -0700

    Third chapter code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'products/:productId', component: ProductDetailComponent}
+ ];
 
-const routes: Routes = [];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
